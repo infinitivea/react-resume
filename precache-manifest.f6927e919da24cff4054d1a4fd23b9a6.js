7f9fed4957f03206a7ee1649337c1b28d823248a
BallStripz <infinitivea@gmail.com>
2020-11-07 21:20:34 +0700

    Updates

---
+++ precache-manifest.f6927e919da24cff4054d1a4fd23b9a6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d80ae1f271a5c400500b12a3a413f565",
+    "url": "/react-resume/index.html"
+  },
+  {
+    "revision": "68d6b0051f82d521b7cd",
+    "url": "/react-resume/static/css/2.e10332d9.chunk.css"
+  },
+  {
+    "revision": "ef0173755355b1091758",
+    "url": "/react-resume/static/css/main.8a46ab90.chunk.css"
+  },
+  {
+    "revision": "68d6b0051f82d521b7cd",
+    "url": "/react-resume/static/js/2.77fdca7b.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/react-resume/static/js/2.77fdca7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef0173755355b1091758",
+    "url": "/react-resume/static/js/main.6ed80400.chunk.js"
+  },
+  {
+    "revision": "7f53f61ad472ba31718b",
+    "url": "/react-resume/static/js/runtime-main.b82ab2ef.js"
+  },
+  {
+    "revision": "068dd4994c27a342dc64a952b18c1fbe",
+    "url": "/react-resume/static/media/codecamp7 v1_201107_1.068dd499.jpg"
+  }
+]);
